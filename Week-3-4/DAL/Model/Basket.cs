174f0185f9b5b87d5bb4f307a1ad81bdb240dbba
nurhayatketenci <nurhayatktnn8@gmail.com>
2022-01-25 02:48:21 +0300

    week 4 update

---
+++ Week-3-4/DAL/Model/Basket.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Model
+{
+    public class Basket
+    {
+        public int Id { get; set; }
+        public int Total_Price { get; set; }
+    }
+}
---
+++ Week-3-4/Entities/UserContext.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DAL.Model;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+namespace Entities
+{
+    public class UserContext:DbContext
+    {
+
+        public UserContext()
+        {
+        }
+        protected readonly IConfiguration Configuration;
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            options.UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=Shopping;Trusted_Connection=true");
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<User>().ToTable("User");
+            modelBuilder.Entity<Adress>().ToTable("Adress");
+            modelBuilder.Entity<APIAuthority>().ToTable("APIAuthority");
+            modelBuilder.Entity<Basket>().ToTable("Basket");
+            modelBuilder.Entity<Product>().ToTable("Product");
+
+
+
+        }
+        public DbSet<User> User { get; set; }
+        public DbSet<Adress> Adress { get; set; }
+        public DbSet<APIAuthority> APIAuthority { get; set; }
+        public DbSet<Product> Product { get; set; }
+        public DbSet<Basket> Basket { get; set; }
+
+    }
+}
